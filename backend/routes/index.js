f80a52ad408dd5c20431542edf34577fea8c948f
Bruzhmelev Anton <bruzhmelev@gmail.com>
2018-09-16 13:07:26 +0300

    prefix changed to api/v1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = router => {
-  router.prefix('/v1');
+  router.prefix('/api/v1');
   router.use('/users', require('./users'));
   router.use('/players', require('./players'));
   router.use('/auth', require('./auth'));
