12e7b2b7d9f0862a73f69a06f453c43d359dda69
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2018-09-19 22:35:53 +0300

    Revert "Revert "Merge pull request #3 from bruzhmelev/ts-node""
    This reverts commit 48e7afd08d822991ccb3eba7061bc59ea9cb9aae.

---
+++ backend/src/domain/content/entities/roads.js
@@ -0,0 +1,18 @@
+export const roads = {
+        pizzeria_malina:        {title: '', sourceId: 'pizzeria', targetId: 'malina', duration: 7},
+        pizzeria_hostel:        {title: '', sourceId: 'pizzeria', targetId: 'hostel', duration: 5},
+        pizzeria_srcrict:      {title: '', sourceId: 'pizzeria', targetId: 'srcrict', duration: 8},
+
+        malina_hostel:          {title: '', sourceId: 'malina', targetId: 'hostel', duration: 10},
+        malina_luber:           {title: '', sourceId: 'malina', targetId: 'luber', duration: 7},
+
+        hostel_hospital:        {title: '', sourceId: 'hostel', targetId: 'hospital', duration: 3},
+
+        hospital_luber:         {title: '', sourceId: 'hospital', targetId: 'luber', duration: 17},
+        hospital_coworking:     {title: '', sourceId: 'hospital', targetId: 'coworking', duration: 12},
+        hospital_srcrict:      {title: '', sourceId: 'hospital', targetId: 'srcrict', duration: 3},
+
+        luber_coworking:        {title: '', sourceId: 'luber', targetId: 'coworking', duration: 10},
+
+        coworking_srcrict:     {title: '', sourceId: 'coworking', targetId: 'srcrict', duration: 6}
+    };
---
+++ backend/src/domain/content/entities/stages.js
@@ -0,0 +1,9 @@
+export const stages = {
+        init: {title: 'Инициация'},
+        home: {title: 'В пиццерии'},
+        orders: {title: 'Набор заказов'},
+        road: {title: 'В дороге'},
+        location: {title: 'В локации'},
+        client: {title: 'У клиента'},
+        map: {title: 'Карта'}
+    };
